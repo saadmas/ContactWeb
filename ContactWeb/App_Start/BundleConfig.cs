a7538e51d9d626d680f172844804544b3077afaf
Saad <saadmasood96@hotmail.com>
2018-12-26 18:27:32 -0500

    setup and config: folder structure, jquery, bootstrap, bootswatch, datatables, site.css, bundle config, layout.cshtml

--- ContactWeb/App_Start/BundleConfig.cs
+++ ContactWeb/App_Start/BundleConfig.cs
@@ -9,22 +9,23 @@ namespace ContactWeb
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/js/lib/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/js/lib/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/js/lib/jquery.dataTables.min.js",
+                        "~/js/lib/dataTables.bootstrap.min.js",
+                        "~/js/lib/dataTables.colReorder.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/css/bootstrap.min.css",          
+                      "~/css/jquery.dataTables.min.css",
+                      "~/css/jquery.dataTables_themeroller.css",
+                      "~/css/dataTables.bootstrap.min.css",
+                      "~/css/colReorder.bootstrap.min.css",
+                      "~/css/site.css"));
         }
     }
 }
